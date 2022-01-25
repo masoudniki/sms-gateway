513d0ce62f972ae6e1396ce6c0254c0a5988f59c
masoud niki <masoud.niki79@gmail.com>
2022-01-25 23:13:30 +0330

    load and register module dependencies

--- Modules/SMS/src/Providers/SMSServiceProvider.php
+++ Modules/SMS/src/Providers/SMSServiceProvider.php
@@ -2,27 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class SMSServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        $this->loadConfig();
+        $this->loadMigrations();
+        $this->loadRoutes();
+        $this->loadTranslations();
+
     }
+    private function loadConfig(){
+        $this->mergeConfigFrom(dirname(__DIR__) . DIRECTORY_SEPARATOR."config/sms.php","sms");
+    }
+    private function loadMigrations(){
+        $this->loadMigrationsFrom(dirname(__DIR__) . DIRECTORY_SEPARATOR."Database/migrations");
+    }
+    private function loadTranslations(){
+        $this->loadTranslationsFrom(dirname(__DIR__) . DIRECTORY_SEPARATOR."resources/lang","sms");
+    }
+    private function loadRoutes(){
+        Route::middleware('api')->namespace('SMS\Http\Controllers')
+            ->group(__DIR__.'/../routes/api.php');
+    }
+
 }
