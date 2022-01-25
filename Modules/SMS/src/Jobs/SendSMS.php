45139a418d5b26f843dabe6d51177c983865fd38
masoud niki <masoud.niki79@gmail.com>
2022-01-26 01:02:21 +0330

    create job for running sms tasks in queue

--- Modules/SMS/src/Jobs/SendSMS.php
+++ Modules/SMS/src/Jobs/SendSMS.php
@@ -8,26 +8,15 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use MODULES\SMS\Drivers\SMS;
 
 class SendSMS implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
+    public function __construct(
+        public SMS $SMS
+    ){}
     public function handle()
     {
         //
