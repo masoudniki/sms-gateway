5a2451604e6e3080200c187b22d4a57148f53ef6
masoud niki <masoud.niki79@gmail.com>
2022-01-26 18:23:20 +0330

    add tests for checking correct driver will create from sms facade

---
+++ Modules/SMS/tests/Feature/Drivers/CheckDriversTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use MODULES\SMS\Drivers\Ghasedak\Ghasedak;
+use MODULES\SMS\Drivers\Kavenegar\Kavenegar;
+use MODULES\SMS\Facade\SMS;
+use Tests\TestCase;
+
+class CheckDriversTest extends TestCase
+{
+    public function test_get_ghasedak_driver_from_sms_facade(){
+        $this->app['config']->set('sms.driver', 'ghasedak');
+        $driver=SMS::getFacadeRoot();
+        $this->assertInstanceOf(Ghasedak::class,$driver);
+    }
+    public function test_get_kavenegar_driver_from_sms_facade(){
+        $this->app['config']->set('sms.driver', 'kavenegar');
+        $driver=SMS::getFacadeRoot();
+        $this->assertInstanceOf(Kavenegar::class,$driver);
+    }
+}
