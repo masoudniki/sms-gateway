3790115dfc0dcc02a16622e7ea2cac8bc5d48984
masoud niki <masoud.niki79@gmail.com>
2022-01-26 19:34:07 +0330

    remove debug code

--- Modules/SMS/src/Http/Controllers/SMSController.php
+++ Modules/SMS/src/Http/Controllers/SMSController.php
@@ -12,7 +12,6 @@ use MODULES\SMS\Models\SMS;
 class SMSController extends Controller
 {
     public function index(){
-        \MODULES\SMS\Facade\SMS::send("09101490337","this is a simple text");
         return new SMSCollection(SMS::query()
             ->paginate(15)
         );
