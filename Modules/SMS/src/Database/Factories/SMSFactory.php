177737b7b4d2da6c31d926a86b7a1e27c392407b
masoud niki <masoud.niki79@gmail.com>
2022-01-26 15:54:44 +0330

    set related model

--- Modules/SMS/src/Database/Factories/SMSFactory.php
+++ Modules/SMS/src/Database/Factories/SMSFactory.php
@@ -3,9 +3,11 @@
 namespace MODULES\SMS\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use MODULES\SMS\Models\SMS;
 
 class SMSFactory extends Factory
 {
+    protected $model=SMS::class;
     /**
      * Define the model's default state.
      *
@@ -14,7 +16,10 @@ class SMSFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "number"=>$this->faker->phoneNumber(),
+            "body"=>$this->faker->realText(500),
+            "status"=>$this->faker->shuffleArray(["failed","sent","sending"])[0],
+            "provider"=>$this->faker->shuffleArray(["kavenegar","ghasedak"])
         ];
     }
 }
