6788a0810203caa911d6272048bfe0f5fdd3a58a
masoud niki <masoud.niki79@gmail.com>
2022-01-26 15:53:02 +0330

    tests for checking job added

---
+++ Modules/SMS/tests/Feature/Job/SendSMSJobTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use MODULES\SMS\Database\Factories\SMSFactory;
+use MODULES\SMS\Facade\SMS;
+use MODULES\SMS\Jobs\SendSMS;
+use Tests\TestCase;
+
+class SendSMSJobTest extends TestCase
+{
+
+    public function test_sms_sent_successfully()
+    {
+        Event::fake();
+        $sms=SMSFactory::times(1)->create()->first();
+        SMS::shouldReceive("send")
+            ->withAnyArgs()
+            ->andReturnTrue();
+        (new SendSMS($sms))->handle();
+        $sms=$sms->fresh();
+        $this->assertEquals(\MODULES\SMS\Models\SMS::SENT,$sms->status);
+    }
+    public function test_sms_failed(){
+        Event::fake();
+        $sms=SMSFactory::times(1)->create()->first();
+        SMS::shouldReceive("send")
+            ->withAnyArgs()
+            ->andThrow(\Exception::class,"something went wrong");
+        (new SendSMS($sms))->handle();
+        $sms=$sms->fresh();
+        $this->assertEquals(\MODULES\SMS\Models\SMS::FAILED,$sms->status);
+        $this->assertEquals("something went wrong",$sms->error_message);
+    }
+}
