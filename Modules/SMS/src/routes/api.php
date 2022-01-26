1fe86538360fdf6c6ab6bfafc97f2317aae71c6a
masoud niki <masoud.niki79@gmail.com>
2022-01-26 13:11:28 +0330

    define routes

--- Modules/SMS/src/routes/api.php
+++ Modules/SMS/src/routes/api.php
@@ -1,1 +1,20 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(["prefix"=>"v1"],function (){
+    Route::group(["prefix"=>"sms"],function (){
+        Route::get("/",[
+            "uses"=>"SMSController@index",
+            "as"=>"sms.index"
+        ]);
+        Route::get("/{sms}",[
+            "uses"=>"SMSController@show",
+            "as"=>"sms.show"
+        ]);
+        Route::post("/submit",[
+            "uses"=>"SMSController@submit",
+            "as"=>"sms.submit"
+        ]);
+    });
+});
